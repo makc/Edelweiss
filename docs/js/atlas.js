makc/Edelweiss

commit 3c0180d1715b04879f4814c7ccb9d6b5081ef9d6

multiple characters support (wip)
introduce assetManager.createCharacter();
work towards multiple CharaAnim objects;
pre-load hero.glb to cut some corners

docs/js/atlas.js
@@ -24,8 +24,6 @@ function Atlas() {
     const NEEDPLANES = false ; // show helpers for limit planes
     const NEED_WATER_HELPER = false ;
 
-    const SCALECHARA = 0.075 ;
-
     const CUBE_INTERSECTION_OFFSET = 0.001 ;
 
 
@@ -450,50 +448,13 @@ function Atlas() {
 		};
 
 
+		let { model, actions } = assetManager.createCharacter();
 
-
-		gltfLoader.load('assets/models/hero.glb', (glb)=> {
-
-
-			let model = glb.scene ;
-			model.scale.set( SCALECHARA, SCALECHARA, SCALECHARA );
 			charaGroup.add( model );
-
-
-			model.traverse( (obj)=> {
-
-				if ( obj.type == 'Mesh' ||
-					 obj.type == 'SkinnedMesh' ) {
-
-					obj.material = new THREE.MeshLambertMaterial({
-						map: obj.material.map,
-						side: THREE.FrontSide,
-						skinning: true
-					});
-
-					obj.castShadow = true ;
-					obj.receiveShadow = true ;
-
-				};
-
-			});
-
-
-			// get the glider object, and give it to the animation
-			// module, the hide it from the scene.
-			charaAnim.setGlider( model.getObjectByName('glider') );
 
 			/// ANIMATIONS
 
-			mixer = new THREE.AnimationMixer( glb.scene );
-
-			glb.animations.forEach( (animClip)=> {
-				actions[ animClip.name ] = mixer.clipAction( animClip );
-			});
-
-			// start all the actions but set their weight to 0
-			for ( let i of Object.keys( actions ) ) {
-				actions[ i ].play() ;
+			for ( let i in actions ) {
 				actions[ i ].setEffectiveWeight( 0 );
 			};
 
@@ -504,14 +465,10 @@ function Atlas() {
 			// the glider is not visible when not in use
 			actions.gliderAction.setEffectiveWeight( 1 );
 
-			// Set the speed of all the actions
-			charaAnim.initActions();
-
-		});
-
 
 
 		return {
+			actions,
 			group,
 			charaGroup,
 			position

docs/js/charaAnim.js
@@ -2,9 +2,12 @@
 
 function CharaAnim( player ) {
 
-	var glider;
-
+    const actions = player.actions ;
     const group = player.charaGroup ;
+
+    // get the glider object, and give it to the animation
+    // module, the hide it from the scene.
+    const glider = group.getObjectByName( 'glider' ); glider.visible = false;
 
 
     // this variable stock the state waiting to be played
@@ -16,7 +19,6 @@ function CharaAnim( player ) {
 
     // This is called when atlas finished loading all the assets.
     // It configures every action.
-    function initActions() {
 
     	climbingActions = [
     		actions.climbUp,
@@ -66,8 +68,6 @@ function CharaAnim( player ) {
         setLoopOnce( actions.dashRight );
         setLoopOnce( actions.dashDownLeft );
         setLoopOnce( actions.dashDownRight );
-
-    };
 
 
     function setLoopOnce( action ) {
@@ -788,13 +788,6 @@ function CharaAnim( player ) {
 
 
 
-    function setGlider( gliderMesh ) {
-    	glider = gliderMesh ;
-    	glider.visible = false ;
-    };
-
-
-
     ///////////////////////////
     ///  ACTIONS SETTING
     ///////////////////////////
@@ -902,8 +895,6 @@ function CharaAnim( player ) {
 
 
     return {
-    	setGlider,
-    	initActions,
     	update,
         setCharaRot,
         group,

docs/js/main.js
@@ -1,14 +1,12 @@
 
 var scene, camera, stats, input, atlas,
     orbitControls, controler, clock, datGUI, charaAnim,
-    gltfLoader, mixer, cameraControl, stamina, interaction,
+    gltfLoader, cameraControl, stamina, interaction,
     dynamicItems, textureLoader, fileLoader, mapManager,
     socketIO, optimizer, uaParser, gameState, feedback,
     assetManager, soundMixer ;
 
 var renderer, composer, fxaaPass ;
-
-var actions = [];
 
 var utils = Utils();
 var easing = Easing();

docs/js/AssetManager.js
@@ -10,6 +10,7 @@ function AssetManager() {
 	// assets constants
 	const SCALE_ALPINIST = 0.1 ;
 	const SCALE_LADY = 0.08 ;
+	const SCALE_CHAR = 0.075 ;
 	const SCALE_EDELWEISS = 0.02 ;
 
 	const OFFSET_ALPINIST = new THREE.Vector3( 0, -0.5, 0 );
@@ -24,12 +25,14 @@ function AssetManager() {
 	// Hold one mixer and one action per asset iteration
 	var alpinistMixers = [], alpinistIdles = [];
 	var ladyMixers = [], ladyIdles = [];
+	var charMixers = [], charActions = [];
 
 	// Asset groups arrays
 	var alpinists = [];
 	var edelweisses = [];
 	var ladies = [];
 	var bonuses = [];
+	var characters = [];
 
 
 
@@ -49,6 +52,7 @@ function AssetManager() {
 	addGroups( edelweisses, 7 );
 	addGroups( ladies, 12 );
 	addGroups( bonuses, 9 );
+	addGroups( characters, 4 );
 
 	function addGroups( arr, groupsNumber ) {
 
@@ -161,14 +165,31 @@ function AssetManager() {
 
 	});
 
+	var charGlb;
+
+	gltfLoader.load('assets/models/hero.glb', (glb)=> {
+
+		charGlb = glb;
+
+		createMultipleModels(
+			glb,
+			SCALE_CHAR,
+			null,
+			characters,
+			charMixers,
+			charActions
+		);
+
+	});
+
 	// Create iterations of the same loaded asset. nasty because of skeletons.
 	// Hopefully THREE.SkeletonUtils.clone() is able to clone skeletons correctly.
 	function createMultipleModels( glb, scale, offset, modelsArr, mixers, actions ) {
 
 		glb.scene.scale.set( scale, scale, scale );
-		glb.scene.position.add( offset );
+		if ( offset ) glb.scene.position.add( offset );
 
-		for ( let i = 0 ; i < modelsArr.length ; i++ ) {
+		for ( let i = mixers ? mixers.length : 0 ; i < modelsArr.length ; i++ ) {
 
 			let newModel = THREE.SkeletonUtils.clone( glb.scene );
 
@@ -178,8 +199,10 @@ function AssetManager() {
 
 				mixers[ i ] = new THREE.AnimationMixer( newModel );
 
-				actions[ i ] = mixers[ i ].clipAction( glb.animations[ 0 ] );
-				actions[ i ].play();
+				actions[ i ] = { };
+				for ( let clip of glb.animations ) {
+					actions[ i ][ clip.name ] = mixers[ i ].clipAction( clip ).play();
+				}
 
 			};
 
@@ -194,6 +217,36 @@ function AssetManager() {
 
 
 
+	function createCharacter() {
+
+		for ( let i = 0; i < characters.length; i++ ) {
+
+			if ( !characters[ i ].userData.isUsed ) {
+				  characters[ i ].userData.isUsed = true;
+
+				// return both the character and its actions
+				return {
+					model : characters[ i ], actions : charActions[ i ]
+				}
+			}
+
+		}
+
+		// if here, we have exhausted all the characters - make some more
+
+		addGroups( characters, 2 );
+
+		createMultipleModels(
+			charGlb,
+			SCALE_CHAR,
+			null,
+			characters,
+			charMixers,
+			charActions
+		);
+
+		return createCharacter();
+	};
 
 
 
@@ -237,7 +290,7 @@ function AssetManager() {
 		let pos = logicCube.position ;
 		let tag = logicCube.tag ;
 
-		for ( asset of assetArray ) {
+		for ( let asset of assetArray ) {
 
 			if ( !asset.userData.isSet ) {
 
@@ -381,29 +434,35 @@ function AssetManager() {
 
 	function update( delta ) {
 
-		alpinistMixers.forEach( (mixer)=> {
+		for ( let mixer of alpinistMixers ) {
 
 			mixer.update( delta );
 
-		});
+		}
 
-		ladyMixers.forEach( (mixer)=> {
+		for ( let mixer of ladyMixers ) {
 
 			mixer.update( delta );
 
-		});
+		}
 
-		edelweisses.forEach( (edelweissGroup)=> {
+		for ( let mixer of charMixers ) {
+
+			mixer.update( delta );
+
+		}
+
+		for ( let edelweissGroup of edelweisses ) {
 
 			updateBonus( edelweissGroup );
 
-		});
+		}
 
-		bonuses.forEach( (bonusGroup)=> {
+		for ( let bonusGroup of bonuses ) {
 
 			updateBonus( bonusGroup );
 
-		});
+		}
 
 	};
 
@@ -417,7 +476,7 @@ function AssetManager() {
 			group.position.copy( group.userData.initPos );
 			group.position.y += ( Math.sin( Date.now() / 700 ) * 0.08 );
 
-			group.children.forEach( (child)=> {
+			for ( let child of group.children ) {
 
 				if ( child.userData.rotationSpeed ) {
 
@@ -425,7 +484,7 @@ function AssetManager() {
 
 				};
 
-			});
+			};
 
 		};
 
@@ -437,6 +496,7 @@ function AssetManager() {
 
 
 	return {
+		createCharacter,
 		createNewLady,
 		createNewAlpinist,
 		createNewEdelweiss,

docs/js/init.js
@@ -75,7 +75,9 @@ function init() {
 
     clock = new THREE.Clock();
 
-    gltfLoader = new THREE.GLTFLoader();
+    var manager = new THREE.LoadingManager();
+
+    gltfLoader = new THREE.GLTFLoader(manager);
     var dracoLoader = new THREE.DRACOLoader();
 
     dracoLoader.setDecoderPath( 'libs/draco/' )
@@ -86,21 +88,24 @@ function init() {
 
     //
 
-
-    uaParser = new UAParser();
-    socketIO = SocketIO();
-    atlas = Atlas();
-    input = Input();
-    stamina = Stamina();
-    interaction = Interaction();
-    dynamicItems = DynamicItems();
-    mapManager = MapManager();
-    optimizer = Optimizer();
-    gameState = GameState();
     assetManager = AssetManager();
-    soundMixer = SoundMixer();
 
+    manager.onLoad = function() {
+        manager.onLoad = function() {};
 
-    loop();
+        uaParser = new UAParser();
+        socketIO = SocketIO();
+        atlas = Atlas();
+        input = Input();
+        stamina = Stamina();
+        interaction = Interaction();
+        dynamicItems = DynamicItems();
+        mapManager = MapManager();
+        optimizer = Optimizer();
+        gameState = GameState();
+        soundMixer = SoundMixer();
+
+        loop();
+    };
 
 };

docs/js/loop.js
@@ -79,7 +79,6 @@ function loop() {
 
     if ( orbitControls ) orbitControls.update();
 
-    if ( mixer ) mixer.update( clockDelta );
     if ( assetManager ) assetManager.update( clockDelta );
 
     if ( charaAnim ) charaAnim.update( clockDelta );

commit cd19c9d56dc82a52f7e3b62615720d81be4f7654

maintenance 🛠
work around minor issues introduced in 3c0180d
debug animation when other player dies a little bit more: d6c581a

docs/js/atlas.js
@@ -1548,7 +1548,7 @@ function Atlas() {
 
 				if ( respawn ) {
 
-					player.position.copy( gameState.respawnPos );
+					respawn();
 
 				} else {
 

docs/js/charaAnim.js
@@ -359,13 +359,13 @@ function CharaAnim( player ) {
 
     	// Delete the starting action from the fadeOut list,
     	// or it would fadein and fadeout at the same time.
-		actionsToFadeOut.forEach( (action, i)=> {
+		for( let i = actionsToFadeOut.length -1; i > -1; i-- ) {
 
-			if ( action.actionName == actionName ) {
+			if ( actionsToFadeOut[ i ].actionName == actionName ) {
 				actionsToFadeOut.splice( i, 1 );
 			};
 
-		});
+		}
 
     };
 
@@ -380,13 +380,13 @@ function CharaAnim( player ) {
 
 		// Delete the starting action from the fadeIn list,
     	// or it would fadein and fadeout at the same time.
-		actionsToFadeIn.forEach( (action, i)=> {
+		for( let i = actionsToFadeIn.length -1; i > -1; i-- ) {
 
-			if ( action.actionName == actionName ) {
+			if ( actionsToFadeIn[ i ].actionName == actionName ) {
 				actionsToFadeIn.splice( i, 1 );
 			};
 
-		});
+		}
 
     };
 
@@ -419,14 +419,8 @@ function CharaAnim( player ) {
 
         //
 
-        if ( newState == 'dying' ) {
-
-            if ( currentState == 'dying' ) return
-
-            setTimeout( ()=> {
-                setState( 'respawn' );
-            }, 1500 );
-
+        if ( currentState != "dying" && newState == 'respawn' ) {
+            return
         };
 
         if ( currentState == "dying" && newState != 'respawn' ) {
@@ -478,7 +472,11 @@ function CharaAnim( player ) {
 					break;
 					
 				case 'dying' :
+					actions.die.reset();
 					setFadeIn( 'die', 1, 1 );
+					setTimeout( function () {
+						setState( 'respawn' ); // remote players plan B
+					}, 1500 );
 					break;
 
     			case 'slipping' :
@@ -865,9 +863,6 @@ function CharaAnim( player ) {
             case 'dashing':
                 dash( data.f || undefined, data.m );
                 break;
-            case 'hittingGround':
-                hitGround();
-                break;
             default:
                 setState( data.a );
                 break;
@@ -971,7 +966,6 @@ function CharaAnim( player ) {
 
 
     function hitGround() {
-		groundHit = false ;
 		setState('hittingGround');
     };
 

docs/js/gameState.js
@@ -342,20 +342,25 @@ function GameState() {
 
         soundMixer.animStart();
 
-        setTimeout( ()=> {
+        setTimeout( function() {
 
             if ( atlas.getSceneGraph() != sceneGraphs.mountain ) {
 
-                atlas.switchGraph( 'mountain', null, true );
+                atlas.switchGraph( 'mountain', null, respawn );
 
-            };
+            } else {
+
+                setTimeout( respawn, 1300 );
+
+            }
 
         }, 250 );
 
-		setTimeout( ()=> {
+    };
 
-			// charaAnim.respawn();
+	function respawn() {
 
+            charaAnim.respawn();
             soundMixer.animEnd();
 
 			atlas.player.position.copy( respawnPos );
@@ -368,8 +373,6 @@ function GameState() {
 
 			domBlackScreen.classList.remove( 'show-black-screen' );
 			domBlackScreen.classList.add( 'hide-black-screen' );
-
-		}, 1500);
 
 	};
 

docs/js/main.js
@@ -36,36 +36,38 @@ function onWindowResize() {
 
     if ( cameraControl ) cameraControl.adaptFOV() ;
 
-    let world = document.getElementById( 'black-screen' );
+    if ( camera ) {
 
-	camera.aspect = world.clientWidth / world.clientHeight;
-	camera.updateProjectionMatrix();
+        let world = document.getElementById( 'black-screen' );
 
-    renderer.setSize( world.clientWidth, world.clientHeight );
+        camera.aspect = world.clientWidth / world.clientHeight;
+        camera.updateProjectionMatrix();
 
-    //
+        renderer.setSize( world.clientWidth, world.clientHeight );
 
-    composer.setSize( world.clientWidth, world.clientHeight );
+        //
 
-    var pixelRatio = renderer.getPixelRatio();
+        composer.setSize( world.clientWidth, world.clientHeight );
 
-    fxaaPass.material.uniforms[ 'resolution' ].value.x = 1 / ( world.clientWidth * pixelRatio );
-    fxaaPass.material.uniforms[ 'resolution' ].value.y = 1 / ( world.clientHeight * pixelRatio );
+        var pixelRatio = renderer.getPixelRatio();
 
-    //
+        fxaaPass.material.uniforms[ 'resolution' ].value.x = 1 / ( world.clientWidth * pixelRatio );
+        fxaaPass.material.uniforms[ 'resolution' ].value.y = 1 / ( world.clientHeight * pixelRatio );
 
-    if ( input.joystick ) {
+        //
 
-        document.getElementById( 'cross' ).style.top =
-                            `${ world.clientHeight - 127.5 }px` ;
-                           
+        if ( input && input.joystick ) {
 
-        input.joystick._baseX = 90 ;
-        input.joystick._baseY = world.clientHeight - 90 ;
+            document.getElementById( 'cross' ).style.top =
+                                `${ world.clientHeight - 127.5 }px` ;
+                               
 
-        input.joystick._baseEl.style.top = 
-            `${ world.clientHeight - ( 90 + ( input.joystick._baseEl.clientHeight / 2 ) ) }px` ;
+            input.joystick._baseX = 90 ;
+            input.joystick._baseY = world.clientHeight - 90 ;
 
-    };
+            input.joystick._baseEl.style.top = 
+                `${ world.clientHeight - ( 90 + ( input.joystick._baseEl.clientHeight / 2 ) ) }px` ;
 
+        }
+    }
 };